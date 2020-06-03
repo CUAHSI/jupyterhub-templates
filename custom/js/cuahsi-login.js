396269b6b15930a8e66e1fadab8a02a9b67d7234
Castronova <castronova.anthony@gmail.com>
2020-06-03 16:15:11 -0400

    fixed checkbox logic

--- custom/js/cuahsi-login.js
+++ custom/js/cuahsi-login.js
@@ -3,7 +3,7 @@ $(document).ready ( function(){
     // disable the sign-in button
     $('#btn-login').addClass('disabled')
     $('#chk-tou').on('change', function() {
-	if ( $('#chk_tou').is(':checked') ) {
+	if ( !$('#chk_tou').is(':checked') ) {
 	    $('#btn-login').addClass('disabled')
 	} else {
 	    $('#btn-login').removeClass('disabled')
